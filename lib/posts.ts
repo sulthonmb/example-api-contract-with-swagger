c9ce9ad6f97f890a6ea6c2de08147f84098a0139
Sulthon <sulthonmb@gmail.com>
2023-09-12 15:08:09 +0700

    Update

--- lib/posts.ts
+++ lib/posts.ts
@@ -40,33 +40,4 @@ export const postApi = c.router({
     },
     summary: "Get a post by id",
   },
-  getPosts: {
-    method: "GET",
-    path: `/posts`,
-    responses: {
-      200: z.array(PostSchema),
-    },
-    summary: "Get all posts",
-  },
-  updatePost: {
-    method: "PATCH",
-    path: `/posts/:id`,
-    responses: {
-      200: PostSchema,
-    },
-    body: z.object({
-      title: z.string().optional(),
-      body: z.string().optional(),
-    }),
-    summary: "Update a post by id",
-  },
-  deletePost: {
-    method: "DELETE",
-    path: `/posts/:id`,
-    responses: {
-      200: PostSchema.nullable(),
-    },
-    body: null,
-    summary: "Delete a post by id",
-  },
 });
